fd31ecaa6f8f023b9999b979bd9a960922e01dee
tcarrio <tom@carrio.me>
2017-04-09 19:44:40 -0400

    Templates for all basic functionality, sessions

--- python/serve.py
+++ python/serve.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import redirect
 from flask import url_for
+from flask import request
 import MySQLdb
 
 app = Flask(__name__)
@@ -15,24 +16,30 @@ db = MySQLdb.connect(
 cur = db.cursor()
 
 @app.route("/")
+def root():
+    global _session
+    return redirect(url_for('index'))
+
+@app.route("/index")
 def index():
     global _session
     if(_session.has_key('authenticated')):
-        return render_template('home.html')
+        return render_template('home.html',sesh=_session)
     else:
         # not logged in, show login page
-        return render_template('welcome.html')
+        return render_template('welcome.html',sesh=_session)
 
 @app.route("/login", methods=['POST'])
-def login(customer=None,password=None):
+def login():
     global _session
     # check database for customerID
-    if(user_in_db(customer)==None):
-        return render_template('error.html',error='Incorrect login information')
-    else:
+    customer=request.args.get('login')
+    if(customer!="" and user_in_db(customer)):
         # set _session variables
         _session['authenticated']=True
-        return redirect(url_for('home'))
+        return redirect(url_for('home'))        
+    else:
+        return render_template('error.html',error='Incorrect login information',sesh=_session)
 
 @app.route("/home")
 def home():
@@ -41,8 +48,7 @@ def home():
     if(auth_check):
         return auth_check
     # continue on if authenticated
-    return render_template('home.html')
-        
+    return render_template('home.html',sesh=_session)
 
 @app.route("/packages")
 @app.route("/packages/<packgeID>")
@@ -54,16 +60,20 @@ def packages(packageID=None):
     # continue on if authenticated
     if(packageID==None):
         customer_packages=get_customer_packages() # use _session['customerID']
-        return render_template('packages.html',packages=customer_packages)
+        return render_template('packages.html',packages=customer_packages,sesh=_session)
     else:
         tracking_info=get_specific_package() # use given packageID
-        return render_template('packages.html',tracking=tracking_info)
+        return render_template('packages.html',tracking=tracking_info,sesh=_session)
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html',sesh=_session)
 
-
+@app.route("/logout")
+def logout():
+    global _session
+    _session = {}
+    return redirect(url_for('index'))
 
 def get_customer_packages(packageID=None):
     return []
@@ -74,4 +84,4 @@ def user_in_db(c):
 def check_authenticated():
     global _session
     if not _session.has_key('authenticated'):
-        return redirect(url_for('login'))
+        return redirect(url_for('index'))
